ed30460116ff7579aafd2cb69aaee721c17b138d
sahalmahin <samirsatter0142@gmail.com>
2023-12-24 20:02:23 +0600

    Put operation is implemented inside AddBlog

--- index.js
+++ index.js
@@ -26,10 +26,6 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         client.connect();
 
-
-
-
-
         // blog collection
         const blogCollection = client.db('blogWebsite').collection('blogs');
         const saveCollection = client.db('blogWebsite').collection('singleBlog');
@@ -47,6 +43,24 @@ async function run() {
             console.log(id);
             const query = { _id: new ObjectId(id) };
             const result = await blogCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.put('/blogs/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedBlog = req.body;
+            const blog = {
+                $set: {
+                    title: updatedBlog.title,
+                    image: updatedBlog.image,
+                    description: updatedBlog.description,
+                    longDescription: updatedBlog.longDescription,
+                    category: updatedBlog.category,
+                }
+            }
+            const result = await blogCollection.updateOne(filter, blog, options);
             res.send(result);
         })
 
@@ -74,7 +88,6 @@ async function run() {
             const result = await saveCollection.deleteOne(query);
             res.send(result);
         })
-
 
         // addBlog
         app.get('/addBlog', async (req, res) => {
@@ -114,16 +127,6 @@ async function run() {
             const result = await addCollection.updateOne(filter, blog, options);
             res.send(result);
         })
-
-
-
-
-
-
-
-
-
-
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
